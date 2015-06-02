2e5c0c458927b2f7f18ff9f754c8919bd745c0f6
mktp <infomarketingpublico@gmail.com>
2015-06-02 13:11:29 -0700

    Ejercicio menu animado

--- public/index.php
+++ public/index.php
@@ -1,4 +1,17 @@
 <?php
+
+/*
+|--------------------------------------------------------------------------
+| Wordpress
+|--------------------------------------------------------------------------
+|
+| Integrate Wordpress with Laravel core
+|
+*/
+
+define('WP_USE_THEMES', false);
+require __DIR__.'/../../blog/wp-blog-header.php';
+
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
